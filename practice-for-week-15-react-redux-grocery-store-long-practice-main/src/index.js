00fe514eede7f180cd633af1f0abd3cdbb2e4e04
swathi <swathibalasubramanyam@gmail.com>
2023-09-27 14:14:30 -0700

    Phase 1 completed

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import configureStore from './store';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production"){
+  window.store = store;
+}
 
 function Root() {
   return (
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
   );
 }
