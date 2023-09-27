dcd936cdd802e08f39f5f62906302648983889f6
Brittiny Filbert <britfilbert@icloud.com>
2023-09-27 14:39:19 -0700

    Added a produceReducer

---
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
@@ -0,0 +1,28 @@
+import produce from '../mockData/produce.json'
+const POPULATE = 'produce/POPULATE'
+
+const produceReducer = (state = {}, action) => {
+    const nextState = Object.assign({}, Object.freeze(state))
+
+
+
+    switch (action.type) {
+        case POPULATE:
+            action.produce.forEach(element => {
+                nextState[element.id] = element
+            });
+
+            return nextState;
+        default:
+            return state;
+    }
+}
+
+
+const populateProduce = () => ({
+    
+    type: POPULATE,
+    produce: produce
+})
+
+export default produceReducer;
